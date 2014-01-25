e2bf29f9a71050130e818339d11e20777d71d958
Francesca Nannizzi <fnannizzi@gmail.com>
2014-01-25 10:09:11 -0800

    Copy of WeatherDemo project. Tagging for reference in case we mess something up later.

---
+++ Kiwi/Kiwi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Kiwi
+//
+//  Created by Francesca Nannizzi on 1/25/14.
+//  Copyright (c) 2014 TeamCocoaPebbles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Kiwi/Kiwi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Kiwi
+//
+//  Created by Francesca Nannizzi on 1/25/14.
+//  Copyright (c) 2014 TeamCocoaPebbles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

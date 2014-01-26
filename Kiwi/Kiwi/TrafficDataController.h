2d33e0e96de8a332c5b1bb07d081c84de30cb8f3
Laurence Wong <laurencerwong@gmail.com>
2014-01-25 17:53:08 -0800

    Added Traffic data controller files

---
+++ Kiwi/Kiwi/TrafficDataController.h
@@ -0,0 +1,13 @@
+//
+//  TrafficDataController.h
+//  Kiwi
+//
+//  Created by Laurence Wong on 1/25/14.
+//  Copyright (c) 2014 TeamCocoaPebbles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TrafficDataController : NSObject
+
+@end
---
+++ Kiwi/Kiwi/TrafficDataController.m
@@ -0,0 +1,15 @@
+//
+//  TrafficDataController.m
+//  Kiwi
+//
+//  Created by Laurence Wong on 1/25/14.
+//  Copyright (c) 2014 TeamCocoaPebbles. All rights reserved.
+//
+
+#import "TrafficDataController.h"
+
+@implementation TrafficDataController
+
+
+
+@end
